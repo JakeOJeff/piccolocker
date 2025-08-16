9e48461e2c37432accb70c5770f804522e9ab111
JakeOJeff <realjakeojeff@gmail.com>
2025-08-16 23:57:27 +0530

    Added a physics body

--- src/game.lua
+++ src/game.lua
@@ -1,9 +1,31 @@
 local game = {}
 
 function game.load()
+    world = wf.newWorld(0, 0, true)
+
+    world:setGravity(0, 9.81)
+
+    ball = {}
+    ball.x = 0
+    ball.y = 0
+    ball.xVel = 0
+    ball.yVel = 0
+    ball.body = love.physics.newBody(world, 400, 300, "dynamic")
+    ball.body:setFixedRotation(true)
+    ball.shape = love.physics.newCircleShape(20)
+    ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+    ball:setGravityScale(0)
 end
 
 function game.update(dt)
+
+    world:update(dt)
+
+end
+
+function game.syncPhysics()
+    ball.x, ball.y = ball.body:getPosition()
+    ball.xVel, ball.yVel = ball.body:getLinearVelocity()
 end
 
 function game.draw()
